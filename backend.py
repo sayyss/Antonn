948b05a680900e486a16ad48c8d704bc3fbf5337
sayamss <sawaisayam@gmail.com>
2020-08-26 01:15:30 +0000

    Basic Dashboard Setup

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
+from operator import itemgetter
 import db_commands
 
 app = Flask(__name__)
@@ -14,7 +14,15 @@ def dashbord():
     guildID = request.args.get('ID')
     guildData = db.getAll(int(guildID))
 
-    return render_template("dashboard.html",guildData=guildData)
+    activeMem = guildData['members']
+    SortedActiveMem = sorted(activeMem, key=itemgetter('total_msg'), reverse=True)
+
+    activeCha = guildData['channels']
+    SortedActiveCha = sorted(activeCha, key=itemgetter('total_msg'), reverse=True)
+
+    members = len(guildData['members'])
+
+    return render_template("dashboard.html",guildData=guildData,activeMembers=SortedActiveMem[:10],activeChannels=SortedActiveCha[:10],members=members)
 
 if __name__ == '__main__':
-    app.run(port=4005)
+    app.run(debug=True, port=4005)
