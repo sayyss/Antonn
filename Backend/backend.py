5ac9d1489cbcf10b2b005b2cabf37748d38bd742
sayyss <sawaisayam@gmail.com>
2020-09-05 18:05:34 +0000

    Added Dashboard

--- Backend/backend.py
+++ Backend/backend.py
@@ -4,8 +4,11 @@ from flask import render_template
 from operator import itemgetter
 
 import db_commands
+import utils
+import datetime
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 db = db_commands.DB()
 
@@ -15,15 +18,31 @@ def dashbord():
     guildID = request.args.get('ID')
     guildData = db.getAll(int(guildID))
 
+    avgMsg = utils.getAvgMessage(guildData['time'],guildData['total_msg'])
+
     activeMem = guildData['members']
     SortedActiveMem = sorted(activeMem, key=itemgetter('total_msg'), reverse=True)
 
     activeCha = guildData['channels']
     SortedActiveCha = sorted(activeCha, key=itemgetter('total_msg'), reverse=True)
 
-    members = len(guildData['members'])
+    data = db.getDailyMsgs(int(guildID))
 
-    return render_template("dashboard.html",guildData=guildData,activeMembers=SortedActiveMem[:10],activeChannels=SortedActiveCha[:10],members=members)
+
+    count = guildData['dailyCount']
+    date = utils.timestamp_to_date(datetime.datetime.today().timestamp())
+
+
+    if not data:
+        Msgx = ['0',date]
+        Msgy = [0,count]
+
+    else:
+        Msgx,Msgy = utils.getPlot(data)
+        Msgx.append(date)
+        Msgy.append(count)
+
+    return render_template("dashboard.html",guildData=guildData,avgMsg=int(avgMsg),members=SortedActiveMem[:11],channels=SortedActiveCha[:11],Msgx=Msgx,Msgy=Msgy)
 
 @app.route("/status", methods=['GET'])
 def status():
---
+++ Backend/utils.py
@@ -0,0 +1,57 @@
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+def getAvgMessage(time,totalMsg):
+
+    timeAdded = datetime.datetime.fromtimestamp(time)
+    currentTime = datetime.date.today()
+
+    timeAdded = datetime.date(timeAdded.year,timeAdded.month,timeAdded.day)
+    currentTime = datetime.date(currentTime.year,currentTime.month,currentTime.day)
+
+    days = currentTime - timeAdded
+
+    if days.days == 0:
+        return totalMsg
+
+    else:
+        return totalMsg/days.days
+
+def getDaysSinceAdded(time):
+
+    timeAdded = datetime.datetime.fromtimestamp(time)
+    currentTime = datetime.date.today()
+
+    timeAdded = datetime.date(timeAdded.year,timeAdded.month,timeAdded.day)
+    currentTime = datetime.date(currentTime.year,currentTime.month,currentTime.day)
+
+    days = currentTime - timeAdded
+
+    return days.days
+
+def getPlot(data):
+
+    x = []
+    y = []
+
+    for i in data:
+
+        time = i['time']
+        time = datetime.datetime.fromtimestamp(time)
+        time = time.date()
+        time = str(time)
+
+        x.append(time)
+        y.append(i['count'])
+    
+    return x,y
+
+def timestamp_to_date(time):
+
+    date = datetime.datetime.fromtimestamp(time)
+    date = date.date()
+    date = str(date)
+
+    return date
+    
